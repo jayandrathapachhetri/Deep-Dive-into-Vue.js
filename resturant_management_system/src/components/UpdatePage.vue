f7fe646540e2d72e3f0d168b10b2080d0acf6b10
Jayandra <jayandrathapa5518@gmail.com>
2023-06-10 14:23:27 +0545

    Add alert in UpdateRastaurant Page

--- resturant_management_system/src/components/UpdatePage.vue
+++ resturant_management_system/src/components/UpdatePage.vue
@@ -1,59 +1,79 @@
 <template>
-<AppHeader />
-<h1>Update Resturant Page</h1>
-<form class="update">
-    <input type="text" placeholder="Enter Name" v-model="restaurant.name" name="name">
-    <input type="text" placeholder="Enter Address" v-model="restaurant.address" name="address">
-    <input type="text" placeholder="Enter Contact" v-model="restaurant.contact" name="contact">
-    <button type="button" @click="updateRestaurant">Update Restaurant</button>
-</form>
-</template>
-
-    
-<script>
-import axios from 'axios';
-import AppHeader from './AppHeader.vue';
-export default {
+    <div>
+      <AppHeader />
+      <h1>Update Restaurant Page</h1>
+      <form class="update">
+        <input type="text" placeholder="Enter Name" v-model="restaurant.name" name="name">
+        <input type="text" placeholder="Enter Address" v-model="restaurant.address" name="address">
+        <input type="text" placeholder="Enter Contact" v-model="restaurant.contact" name="contact">
+        <button type="button" @click="showAlertDialog">Update Restaurant</button>
+      </form>
+  
+      <div v-if="showAlert" class="alert">
+        <p>Are you sure you want to update the restaurant information?</p>
+        <div class="button-group">
+          <button @click="keepChanges">Keep Changes</button>
+          <button @click="cancelUpdate">Cancel</button>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  import AppHeader from './AppHeader.vue';
+  export default {
     name: "UpdatePage",
-
+  
     components: {
-        AppHeader
+      AppHeader
     },
+  
     data() {
-        return {
-            restaurant: {
-                name: "",
-                address: "",
-                contact: ""
-            }
-        }
+      return {
+        restaurant: {
+          name: "",
+          address: "",
+          contact: ""
+        },
+        showAlert: false
+      };
     },
+  
     methods: {
-        async updateRestaurant(){
-            // console.warn(this.restaurant)
-            const result = await axios.put("http://localhost:3000/restaurant/"+this.$route.params.id,{
-                name: this.restaurant.name,
-                address: this.restaurant.address,
-                contact: this.restaurant.contact
-            });
-            if(result.status==200){
-                this.$router.push({name:'HomePage'});
-            }
-            // console.warn("function called", this.restaurant)
+      showAlertDialog() {
+        this.showAlert = true;
+      },
+  
+      async updateRestaurant() {
+        const result = await axios.put("http://localhost:3000/restaurant/" + this.$route.params.id, {
+          name: this.restaurant.name,
+          address: this.restaurant.address,
+          contact: this.restaurant.contact
+        });
+        if (result.status === 200) {
+          this.$router.push({ name: 'HomePage' });
         }
-    },  
-
+      },
+  
+      keepChanges() {
+        this.updateRestaurant();
+        this.showAlert = false;
+      },
+  
+      cancelUpdate() {
+        this.showAlert = false;
+      }
+    },
+  
     async mounted() {
-        let user = localStorage.getItem("user-info");
-        if (!user) {
-            this.$router.push({
-                name: 'SignUp'
-            })
-        }
-        let result = await axios.get("http://localhost:3000/restaurant/"+this.$route.params.id);
-        // console.warn(this.$route.params.id)
-        // console.warn(result)
-        this.restaurant = result.data;
+      let user = localStorage.getItem("user-info");
+      if (!user) {
+        this.$router.push({ name: 'SignUp' });
+      }
+      let result = await axios.get("http://localhost:3000/restaurant/" + this.$route.params.id);
+      this.restaurant = result.data;
     }
-}
-</script>
+  };
+  </script>
+  
